6c1fed597705b8d04b715ffc0a8ad4c21f21bb82
Anavarrodo <a.navarrodo@gmail.com>
2023-06-23 12:29:07 +0200

    create searcher component

---
+++ src/components/Searcher.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Searcher = ({ defaultPodcasts, onChange }) => {
+
+    const [ numberLength, setNumberLength ] = useState(defaultPodcasts.length);
+
+    const filterBySearch = ( searchedWord ) => {
+        const filteredPodcasts = defaultPodcasts.filter((podcast) =>
+            podcast.title.label.toLowerCase().includes(searchedWord) ||
+            podcast['im:artist'].label.toLowerCase().includes(searchedWord)
+        );
+    
+        if (filteredPodcasts.length !== 0) {
+            onChange(filteredPodcasts);
+            setNumberLength(filteredPodcasts.length);
+        } else {
+          onChange([]);
+        }
+      };
+
+    return (
+        <Container>
+            <BoxLength>
+                <Number>{ numberLength }</Number>
+            </BoxLength>
+            <SearchBox
+                placeholder="Filter podcasts"
+                onChange={(e) => filterBySearch(e.target.value.toLowerCase())}
+            />
+        </Container>
+    )
+}
+
+export default Searcher;
+
+const Container = styled.div`
+    display: flex;
+    margin-top: 32px;
+`;
+const BoxLength = styled.div`
+    background: lightblue;
+    border-radius: 30%;
+    width: 50px;
+    height: 25px;
+    margin-right: 20px;
+    display: flex;
+    background: #5E97F6;
+`;
+
+const Number = styled.span`
+    margin: auto;
+    font-family: Montserrat-Bold;
+    color: #fefefe;
+`;
+
+const SearchBox = styled.input`
+    border: 1px solid #D1D7DC;
+    border-radius: 3px;
+    font-family: Montserrat-Regular;
+    &:focus {
+        outline: none;
+    }
+`;
--- src/pages/PodcastList.jsx
+++ src/pages/PodcastList.jsx
@@ -1,37 +1,54 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import apiServices from '../services/api';
 import styled from 'styled-components';
 import useLocalStorage from '../hooks/localStorage';
 import Card from '../components/Card';
+import Searcher from '../components/Searcher';
 
 const PodcastList = () => {
 
     const [podcastsOriginal, setPodcastsOriginal] = useLocalStorage('podcasts', []);
+    const [podcastsFilter, setPodcastsFilter] = useState([]);
 
     useEffect(() => {
         podcastsOriginal.length === 0 && getApi();
+        setPodcastsFilter(podcastsOriginal);
     }, []);
 
     const getApi = () => {
         apiServices.getPodcastList()
             .then((podcasts) => {
                 setPodcastsOriginal(podcasts);
+                setPodcastsFilter(podcasts)
             })
             .catch( console.error );
     }
 
+    const getFilterPodcats = ( search ) => {
+        setPodcastsFilter(search);
+    }
+
     return ( 
         <Container>
-            {podcastsOriginal.map((podcast) => 
-                <Card 
-                    key={podcast.id.label} 
-                    img={podcast['im:image'][2].label}
-                    name={podcast['im:name'].label}
-                    artist={podcast['im:artist'].label}
-                    id={podcast.id.attributes['im:id']}
-                />
-            )}
+            <BoxSearcher>
+                <Searcher         
+                    defaultPodcasts={podcastsOriginal}
+                    onChange={ getFilterPodcats }
+                /> 
+            </BoxSearcher>
+            <BoxCards>
+                {podcastsFilter.map((podcast) => 
+                    <Card 
+                        key={podcast.id.label} 
+                        img={podcast['im:image'][2].label}
+                        name={podcast['im:name'].label}
+                        artist={podcast['im:artist'].label}
+                        id={podcast.id.attributes['im:id']}
+                    />
+                )}
+            </BoxCards>
         </Container>
+
     )
 };
 
@@ -39,6 +56,16 @@ export default PodcastList;
 
 const Container = styled.div`
     display: flex;
+    flex-direction: column;
+`;
+
+const BoxSearcher = styled.div`
+    display: flex;
+    justify-content: end;
+`;
+
+const BoxCards = styled.div`
+    display: flex;
     flex-wrap: wrap;
-    margin-top: 75px;
+    margin-top: 32px;
 `;
